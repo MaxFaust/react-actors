171ca5f51fa9e81efb076d32cef38dcd78dbd95c
Max Faust <tcthinker@gmail.com>
2020-04-01 13:19:41 -0500

    handleSubmit works

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
+    state = {
+        user: {
+            firstName: '',
+            lastName: '',
+            bestImpression: ''
+        },
+        userList: []
+    }
+
+    //Handle inputs with conditional rendering and append to DOM
+    handleChangeFor = ( event, propName ) => {
+        console.log(event.target.value, propName);
+        this.setState({
+            user: {
+                ...this.state.user,
+                [propName]: event.target.value
+            }
+        })
+    }
+
+    handleSubmit = () => {
+        console.log ('Submitting:', this.state.user);
+        this.setState( {
+            user: {
+                firstName: '',
+                lastName: '',
+                bestImpression: ''
+            },
+            userList: [...this.state.userList, this.state.user ]
+        })
+    }
+
+
     render() {
         return (
-            <div class="Contact-info">
+            <div>
             <h2>Contact</h2>
-            <input type="text" id="f-name" placeholder="First Name"></input>
-            <input type="text" id="l-name" placeholder="Last Name"></input>
-            <input type="text" id="impression" placeholder="Best Impression"></input>
-            <p id="append"></p>
+            <input type="text" placeholder="First Name" value={this.state.user.firstName}
+            onChange={ (event) =>this.handleChangeFor(event, 'firstName')}></input>
+            <input type="text" placeholder="Last Name" value={this.state.user.lastName}
+            onChange={ (event) =>this.handleChangeFor(event, 'lastName')}></input>
+            <input type="text" placeholder="Best Impression" value={this.state.user.bestImpression}
+            onChange={ (event) =>this.handleChangeFor(event, 'bestImpression')}></input>
+            <button onClick={ this.handleSubmit}>Submit</button>
             <p>Phone: 555-123-4567</p> 
             <p>Email: PowerIsLife@UmbrellaCorp.com</p>
             </div>
