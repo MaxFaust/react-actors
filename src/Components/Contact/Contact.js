7ceca050a3fce9579355578619ee43c19af6afc7
Max Faust <tcthinker@gmail.com>
2020-04-01 14:21:34 -0500

    if/else conditionally renders if no empty inputs

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,7 +11,7 @@ class Footer extends Component {
     }
 
     //Handle inputs with conditional rendering and append to DOM
-    handleChangeFor = ( event, propName ) => {
+    handleChangeFor = (event, propName) => {
         console.log(event.target.value, propName);
         this.setState({
             user: {
@@ -22,37 +22,47 @@ class Footer extends Component {
     }
 
     handleSubmit = () => {
-        console.log ('Submitting:', this.state.user);
-        this.setState( {
-            user: {
-                firstName: '',
-                lastName: '',
-                bestImpression: ''
-            },
-            userList: [...this.state.userList, this.state.user ]
-        })
-    }
-
+        console.log('Submitting:', this.state.user);
+        if (this.state.user.firstName !== '' && this.state.user.lastName !== '' && this.state.user.bestImpression !== '') {
+            this.setState({
+                user: {
+                    firstName: '',
+                    lastName: '',
+                    bestImpression: ''
+                },
+                userList: [...this.state.userList, this.state.user]
+            })
+            alert(`Thanks for joining us ${this.state.user.firstName}`)
+        }
+        else {
+            alert(`Please complete the input form`)
+            }
+        };
 
     render() {
         return (
             <div>
-            <h2>Contact</h2>
-            <input type="text" placeholder="First Name" value={this.state.user.firstName}
-            onChange={ (event) =>this.handleChangeFor(event, 'firstName')}></input>
-            <input type="text" placeholder="Last Name" value={this.state.user.lastName}
-            onChange={ (event) =>this.handleChangeFor(event, 'lastName')}></input>
-            <input type="text" placeholder="Best Impression" value={this.state.user.bestImpression}
-            onChange={ (event) =>this.handleChangeFor(event, 'bestImpression')}></input>
-            <button onClick={ this.handleSubmit}>Submit</button>
-            <p>Phone: 555-123-4567</p> 
-            <p>Email: PowerIsLife@UmbrellaCorp.com</p>
+                <h2>Contact</h2>
+                <input type="text" placeholder="First Name" value={this.state.user.firstName}
+                    onChange={(event) => this.handleChangeFor(event, 'firstName')}></input>
+                <input type="text" placeholder="Last Name" value={this.state.user.lastName}
+                    onChange={(event) => this.handleChangeFor(event, 'lastName')}></input>
+                <input type="text" placeholder="Best Impression" value={this.state.user.bestImpression}
+                    onChange={(event) => this.handleChangeFor(event, 'bestImpression')}></input>
+                <button onClick={this.handleSubmit}>Submit</button>
+                <p>Best Impressions:</p>
+                {/* {JSON.stringify(this.state)} */}
+                <ul>
+                    {this.state.userList.map((item) => <li key={item.firstName}>{item.firstName} {item.lastName}'s best impression is of {item.bestImpression}</li>)}
+                </ul>
+                <p>Phone: 555-123-4567</p>
+                <p>Email: PowerIsLife@UmbrellaCorp.com</p>
             </div>
-            );
-            //TODO- handle inputs with conditional rendering and append to DOM if all inputs !== ''
-            //          Add alert- "Thank You!"
-            //          Form clears after click
-            }
+        );
+        //TODO- handle inputs with conditional rendering and append to DOM if all inputs !== ''
+        //          Add alert- "Thank You!"
+        //          Form clears after click
     }
+}
 
 export default Footer;
